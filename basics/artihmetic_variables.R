e46595d35f4925ea48337d823c8b11740af04126
Paul Torres <pntorres1542@gmail.com>
2020-12-18 12:49:01 -0500

    variables update

---
+++ basics/artihmetic_variables.R
@@ -0,0 +1,41 @@
+# Basics and Base Line Math
+
+# This file is the beginning of my journey into R and its syntax. The first part is arithmetics.
+
+1+1 # = 2
+
+100-101 # = -1
+
+5*10 # = 50
+
+5/3 # = 1.6667
+
+2^4 # = 16
+
+100 * 2 + 54 # = 254
+
+100 * (2+54) # = 5600
+
+5%%2 # = 1
+
+# This part is dedicated to variables
+
+# Variables can be assigned one of two ways. The first is with the equals operator, standard in python too
+
+b = 5
+
+# Another is with the arrow indicator, which is a less than symbol right next to a dash
+
+b <- 6 # This will overwrite the above b variable
+
+# Both ways work
+
+
+
+
+
+
+
+
+
+
