032466e930b17dd2ecb9ae097d2634e259ea34af
Paul Torres <pntorres1542@gmail.com>
2021-01-18 08:50:22 -0500

    operators and indexing

--- basics/comparison_operators_index.R
+++ basics/comparison_operators_index.R
@@ -15,4 +15,29 @@ V = 2
 V > -1
 
 
-# 
+# Vector Indexing and Slicing
+
+v1 = c(100,200,300)
+v2 = c('a','b','c')
+
+# in R the indexing starts at 1, unlike Python which starts at 0
+v1[1]
+
+# this would be the way in Python to grab the first three elements
+v1[1,2] # but does not work in R
+
+# this is the way to get it in R
+v1[c(1,2)]
+
+Slicing
+# same as in Python
+v1[2:4]
+
+# how to name entries
+names(v1) = c(v2)
+
+# now each entry has a name. I can call it by the index or the name
+v1
+
+
+
